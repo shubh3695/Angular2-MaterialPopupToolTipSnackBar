33d158ac3c2c09f4ebdf30d959e6b30fabd9aa76
Shubham Saxena <shubh3695@gmail.com>
2017-10-06 20:08:37 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-
+import { MatDialogModule, MatSnackBarModule, MatCardModule, MatButtonModule, MatTooltipModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { DialogDemoComponent } from './dialog-demo/dialog-demo.component';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
+import { TooltipDemoComponent } from './tooltip-demo/tooltip-demo.component';
+import { SnackbarDemoComponent } from './snackbar-demo/snackbar-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogDemoComponent,
+    MyDialogComponent,
+    TooltipDemoComponent,
+    SnackbarDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatDialogModule,
+    BrowserAnimationsModule, MatButtonModule,
+    MatCardModule, MatTooltipModule,
+    MatSnackBarModule
+  ],
+  entryComponents:[
+    MyDialogComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
